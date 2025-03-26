94c25fdb1f129a01aa3a036dbeea1e0effef4fe0
Sarva197 <168011839+Sarva197@users.noreply.github.com>
2025-03-26 20:31:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: process.env.PORT || 4173,
-    host: true
-  },
-  preview: {
     port: process.env.PORT || 4173,
     host: true,
     allowedHosts: ['to-do-app-with-api-integration.onrender.com']
+  },
+  preview: {
+    port: process.env.PORT || 4173,
+    host: true
   }
 });

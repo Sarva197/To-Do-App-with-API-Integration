764b1b815ae021fdcca3c027ab3d9c57cc309b0a
Sarva197 <168011839+Sarva197@users.noreply.github.com>
2025-03-26 20:22:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ export default defineConfig({
   server: {
     port: process.env.PORT || 4173,
     host: true
+  },
+  preview: {
+    port: process.env.PORT || 4173,
+    host: true,
+    allowedHosts: ['to-do-app-with-api-integration.onrender.com']
   }
 });
